47b56e896e4afe35d5fabc6e143e5421e09babc9
nrjohnstone <locotewz@gmail.com>
2016-03-21 19:43:29 +1300

    initial router/dealer implementation checkin based on netmq docs

---
+++ solution/client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        private static Guid _uuid;
+
+        static void Main(string[] args)
+        {
+            var delay = 1000;
+
+            using (var context = NetMQContext.Create())
+            {
+                using (var dealer = context.CreateDealerSocket())
+                {
+                    using (var poller = new Poller())
+                    {
+                        poller.AddSocket(dealer);
+
+                        _uuid = Guid.NewGuid();
+                        dealer.Options.Identity =
+                            Encoding.Unicode.GetBytes(_uuid.ToString());
+
+                        dealer.Connect("tcp://127.0.0.1:5556");
+                        dealer.ReceiveReady += Client_ReceiveReady;
+
+                        poller.PollTillCancelledNonBlocking();
+
+
+                        while (true)
+                        {
+                            var messageToServer = new NetMQMessage();
+                            messageToServer.AppendEmptyFrame();
+                            messageToServer.Append(_uuid.ToString());
+                            Console.WriteLine("======================================");
+                            Console.WriteLine(" OUTGOING MESSAGE TO SERVER ");
+                            Console.WriteLine("======================================");
+                            PrintFrames("Client Sending", messageToServer);
+                            dealer.SendMultipartMessage(messageToServer);
+
+                            Thread.Sleep(delay);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Client_ReceiveReady(object sender, NetMQSocketEventArgs e)
+        {
+            bool hasmore = false;
+            e.Socket.Receive(out hasmore);
+            if (hasmore)
+            {
+                string result = e.Socket.ReceiveFrameString(out hasmore);
+                string responseUuid = result.Split(new[] {'|'})[0];
+                
+                
+                Console.WriteLine("REPLY {0}", result);
+                if (!responseUuid.Equals(_uuid.ToString()))
+                    throw new Exception("Response UUID does not match client");
+            }
+        }
+
+        static void PrintFrames(string operationType, NetMQMessage message)
+        {
+            for (int i = 0; i < message.FrameCount; i++)
+            {
+                Console.WriteLine("{0} Socket : Frame[{1}] = {2}", operationType, i,
+                    message[i].ConvertToString());
+            }
+        }
+    }
+}
---
+++ solution/server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NetMQ;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = NetMQContext.Create();
+            var server = context.CreateRouterSocket();
+           
+            server.Bind("tcp://*:5556");
+
+            while (true)
+            {
+                NetMQMessage clientMessage = server.ReceiveMultipartMessage();
+                Console.WriteLine("======================================");
+                Console.WriteLine(" INCOMING CLIENT MESSAGE FROM CLIENT ");
+                Console.WriteLine("======================================");
+                PrintFrames("Server receiving", clientMessage);
+                if (clientMessage.FrameCount == 3)
+                {
+                    var clientAddress = clientMessage[0];
+                    var clientOriginalMessage = clientMessage[2].ConvertToString();
+                    string response = string.Format("{0} back from server {1}",
+                        clientOriginalMessage, DateTime.Now.ToLongTimeString());
+                    var messageToClient = new NetMQMessage();
+                    messageToClient.Append(clientAddress);
+                    messageToClient.AppendEmptyFrame();
+                    messageToClient.Append(response);
+                    server.SendMultipartMessage(messageToClient);
+                }
+            }
+
+        }
+
+        static void PrintFrames(string operationType, NetMQMessage message)
+        {
+            for (int i = 0; i < message.FrameCount; i++)
+            {
+                Console.WriteLine("{0} Socket : Frame[{1}] = {2}", operationType, i,
+                    message[i].ConvertToString());
+            }
+        }
+    }
+}
