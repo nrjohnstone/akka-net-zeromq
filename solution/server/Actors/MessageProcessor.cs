ee27290ee7e8d17add31072a6b8ee59c93ba2118
nrjohnstone <locotewz@gmail.com>
2016-03-21 20:12:49 +1300

    refactor server to to use an actor to recieve the request and send the response to the sender

---
+++ solution/server/Actors/MessageProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Akka.Actor;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Server.Actors
+{
+    internal class MessageProcessor : ReceiveActor
+    {
+        public class ProcessMessage
+        {
+            public NetMQMessage ClientMessage { get;}
+            public RouterSocket Server { get; }
+
+            public ProcessMessage(NetMQMessage clientMessage, RouterSocket server)
+            {
+                ClientMessage = clientMessage;
+                Server = server;
+            }
+        }
+
+        public MessageProcessor()
+        {
+            Receive<ProcessMessage>(msg => HandleProcessMessage(msg));
+        }
+
+        private void HandleProcessMessage(ProcessMessage msg)
+        {
+            var clientMessage = msg.ClientMessage;
+            var server = msg.Server;
+
+            var clientAddress = clientMessage[0];
+            var clientOriginalMessage = clientMessage[2].ConvertToString();
+
+            string response = string.Format("{0}| back from server {1}",
+                clientOriginalMessage, DateTime.Now.ToLongTimeString());
+
+            var messageToClient = new NetMQMessage();
+            messageToClient.Append(clientAddress);
+            messageToClient.AppendEmptyFrame();
+            messageToClient.Append(response);
+            server.SendMultipartMessage(messageToClient);
+        }
+    }
+}
--- solution/server/Program.cs
+++ solution/server/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using Akka.Actor;
 using NetMQ;
+using Server.Actors;
 
-namespace server
+namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var actorSystem = ActorSystem.Create("AkkaNetMqExample");
+
+            Props messageProcessorProps = Props.Create(() => new MessageProcessor());
+            IActorRef messageProcessor = actorSystem.ActorOf(messageProcessorProps, "someNonDefaultActor");
+
             var context = NetMQContext.Create();
             var server = context.CreateRouterSocket();
            
@@ -21,18 +28,9 @@ namespace server
                 PrintFrames("Server receiving", clientMessage);
                 if (clientMessage.FrameCount == 3)
                 {
-                    var clientAddress = clientMessage[0];
-                    var clientOriginalMessage = clientMessage[2].ConvertToString();
-                    string response = string.Format("{0} back from server {1}",
-                        clientOriginalMessage, DateTime.Now.ToLongTimeString());
-                    var messageToClient = new NetMQMessage();
-                    messageToClient.Append(clientAddress);
-                    messageToClient.AppendEmptyFrame();
-                    messageToClient.Append(response);
-                    server.SendMultipartMessage(messageToClient);
+                    messageProcessor.Tell(new MessageProcessor.ProcessMessage(clientMessage, server));                    
                 }
             }
-
         }
 
         static void PrintFrames(string operationType, NetMQMessage message)
